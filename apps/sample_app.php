11e2c2e87354ceec504f8733f0300ac2940b971b
Skylar Ittner <admin@netsyms.com>
2017-05-13 17:25:05 -0600

    Update sample app

--- apps/sample_app.php
+++ apps/sample_app.php
@@ -2,8 +2,18 @@
 
 dieifnotloggedin();
 
-$APPS["sample_app"]["title"] = "Sample App";
+// Additional i18n strings
+addMultiLangStrings(["en_us" => [
+        "sample app" => "Sample Application",
+    ]
+]);
+// Set to true to automatically parse the app title as a language string.
+$APPS["sample_app"]["i18n"] = TRUE;
+// App title.
+$APPS["sample_app"]["title"] = "sample app";
+// App icon, from FontAwesome.
 $APPS["sample_app"]["icon"] = "rocket";
+// App content.
 $APPS["sample_app"]["content"] = <<<'CONTENTEND'
 <div class="list-group">
     <div class="list-group-item">
