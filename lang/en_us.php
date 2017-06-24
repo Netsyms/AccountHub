f06a1037705d192eb46ba91cd13b1aee82c6d5f8
Skylar Ittner <admin@netsyms.com>
2017-06-23 18:26:53 -0600

    Improve widget wrapping responsiveness

--- lang/en_us.php
+++ lang/en_us.php
@@ -6,16 +6,20 @@ $STRINGS = [
     "password" => "Password",
     "continue" => "Continue",
     "authcode" => "Authentication code",
-    "2fa prompt" => "Enter the six-digit code from your mobile authenticator app.",
+    "2fa prompt" => "Enter the six-digit code from your mobile authenticator "
+    . "app.",
     "2fa incorrect" => "Authentication code incorrect.",
     "login incorrect" => "Login incorrect.",
     "login successful" => "Login successful.",
     "login error" => "There was a server problem.  Try again later.",
-    "account locked" => "This account has been disabled. Contact technical support.",
+    "account locked" => "This account has been disabled. Contact technical "
+    . "support.",
     "password expired" => "You must change your password before continuing.",
     "account terminated" => "Account terminated.  Access denied.",
-    "account state error" => "Your account state is not stable.  Log out, restart your browser, and try again.",
-    "password on 500 list" => "The given password is ranked number {arg} out of the 500 most common passwords.  Try a different one.",
+    "account state error" => "Your account state is not stable.  Log out, "
+    . "restart your browser, and try again.",
+    "password on 500 list" => "The given password is ranked number {arg} out "
+    . "of the 500 most common passwords.  Try a different one.",
     "welcome user" => "Welcome, {user}!",
     "change password" => "Change password",
     "security options" => "Security options",
@@ -25,7 +29,8 @@ $STRINGS = [
     "options" => "Options",
     "404 error" => "404 Error",
     "page not found" => "Page not found.",
-    "current password incorrect" => "The current password is incorrect.  Try again.",
+    "current password incorrect" => "The current password is incorrect.  "
+    . "Try again.",
     "new password mismatch" => "The new passwords did not match.  Try again.",
     "weak password" => "Password does not meet requirements.",
     "password updated" => "Password updated successfully.",
@@ -33,25 +38,50 @@ $STRINGS = [
     "2fa removed" => "2-factor authentication disabled.",
     "2fa enabled" => "2-factor authentication activated.",
     "remove 2fa" => "Disable 2-factor authentication",
-    "2fa explained" => "2-factor authentication adds more security to your account. You'll need an app such as Google Authenticator on your smartphone. When you have the app installed, you can enable 2-factor authentication by clicking the button below and scanning a QR code with the app. Whenever you sign in in the future, you'll need to input a six-digit code from your phone into the login page when prompted. You can disable 2-factor authentication from this page if you change your mind.",
-    "2fa active" => "2-factor authentication is active on your account.  To remove 2fa, reset your authentication secret, or change to a new security device, click the button below.",
+    "2fa explained" => "2-factor authentication adds more security to your "
+    . "account. You'll need an app such as Google Authenticator on your "
+    . "smartphone. When you have the app installed, you can enable 2-factor "
+    . "authentication by clicking the button below and scanning a QR code with "
+    . "the app. Whenever you sign in in the future, you'll need to input a "
+    . "six-digit code from your phone into the login page when prompted. You "
+    . "can disable 2-factor authentication from this page if you change your "
+    . "mind.",
+    "2fa active" => "2-factor authentication is active on your account.  To "
+    . "remove 2fa, reset your authentication secret, or change to a new "
+    . "security device, click the button below.",
     "enable 2fa" => "Enable 2-factor authentication",
-    "scan 2fa qrcode" => "Scan the QR Code with the authenticator app, or enter the secret key manually.",
+    "scan 2fa qrcode" => "Scan the QR Code with the authenticator app, or enter"
+    . " the secret key manually.",
     "confirm 2fa" => "Finish setup",
     "invalid parameters" => "Invalid request parameters.",
     "ldap server error" => "The LDAP server returned an error: {arg}",
     "user does not exist" => "User does not exist.",
-    "captcha error" => "There was a problem with the CAPTCHA (robot test).  Try again.",
+    "captcha error" => "There was a problem with the CAPTCHA (robot test).  "
+    . "Try again.",
     "home" => "Home",
     "ldap error" => "LDAP error: {error}",
-    "old and new passwords match" => "Your current and new passwords are the same.",
+    "old and new passwords match" => "Your current and new passwords are the "
+    . "same.",
     "generic op error" => "An unknown error occurred.  Try again later.",
-    "password complexity insufficent" => "The new password does not meet the minumum requirements defined by your system administrator.",
+    "password complexity insufficent" => "The new password does not meet the "
+    . "minumum requirements defined by your system administrator.",
     "error loading widget" => "There was a problem loading this app.",
     "open app" => "Open App",
     "sign in again" => "Please sign in again to continue.",
-    "login failed try on web" => "There is a problem with your account. Visit Portal via a web browser for more information.",
-    "mobile login disabled" => "Mobile login has been disabled by your system administrator.  Contact technical support for more information.",
+    "login failed try on web" => "There is a problem with your account. Visit "
+    . "Portal via a web browser for more information.",
+    "mobile login disabled" => "Mobile login has been disabled by your system "
+    . "administrator.  Contact technical support for more information.",
     "admin alert email subject" => "Alert: User login notification",
-    "admin alert email message" => "You (or another administrator) requested to be notified when user \"{username}\" logged in, an event which happened just now.\r\n\r\nUsername: \t{username}\r\nApplication: \t{appname}\r\nDate/Time: \t{datetime}\r\nIP address: \t{ipaddr}\r\n\r\nThese notifications can be disabled by editing the user in ManagePanel.",
+    "admin alert email message" => "You (or another administrator) requested to"
+    . " be notified when user \"{username}\" logged in, an event which happened"
+    . " just now."
+    . "\r\n"
+    . "\r\nUsername: \t{username}"
+    . "\r\nApplication: \t{appname}"
+    . "\r\nDate/Time: \t{datetime}"
+    . "\r\nIP address: \t{ipaddr}"
+    . "\r\n"
+    . "\r\nThese notifications can be disabled by editing the user in "
+    . "ManagePanel.",
 ];
