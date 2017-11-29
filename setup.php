b7c15dc15bb7a1ade53d6d89ec6cdeb4e6fa9866
Skylar Ittner <admin@netsyms.com>
2017-11-28 17:13:34 -0700

    Improve setup script

--- setup.php
+++ setup.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This script will create a local administrator account.
  */
@@ -13,33 +12,28 @@ if ($database->has('accounts', ["[>]assigned_permissions" => ["uid" => "uid"]], 
 if (is_empty($_POST['username']) || is_empty($_POST['password']) || is_empty($_POST['realname'])) {
     ?>
     <!DOCTYPE html>
-    <html>
-        <head>
-            <title>Admin Account Creation</title>
-        </head>
-        <body>
-            <h1>Admin Account Creation tool</h1>
-            <form action="setup.php" method="POST">
-                Username: <input type="text" name="username" placeholder="Username" required="required" /><br />
-                Password: <input type="text" name="password" placeholder="Password" required="required" /><br />
-                Name: <input type="text" name="realname" placeholder="Real Name" required="required" /><br />
-                Email: <input type="email" name="email" placeholder="Email Address" /><br />
-                <button type="submit">
-                    Create account
-                </button>
-            </form>
-        </body>
-    </html>
-<?php
+    <title>Admin Account Creation</title>
+    <h1>Admin Account Creation tool</h1>
+    <form action="setup.php" method="POST">
+        Username: <input type="text" name="username" placeholder="Username" required="required" /><br />
+        Password: <input type="text" name="password" placeholder="Password" required="required" /><br />
+        Name: <input type="text" name="realname" placeholder="Real Name" required="required" /><br />
+        Email: <input type="email" name="email" placeholder="Email Address" /><br />
+        <button type="submit">
+            Create account
+        </button>
+    </form>
+    <?php
 } else {
     require_once __DIR__ . "/lib/login.php";
-    $userid = adduser($_POST['username'],
-            $_POST['password'],
-            $_POST['realname'],
-            (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? $_POST['email'] : null),
-            "",
-            "",
-            1);
+    header("Content-Type: text/plain");
+    if (user_exists($_POST['username'])) {
+        $userid = $database->get('accounts', 'uid', ['username' => $_POST['username']]);
+        echo "User already exists, skipping creation.\n";
+    } else {
+        $userid = adduser($_POST['username'], $_POST['password'], $_POST['realname'], (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ? $_POST['email'] : null), "", "", 1);
+        echo "User account #$userid created.\n";
+    }
     $database->insert('assigned_permissions', ['uid' => $userid, 'permid' => 1]);
-    die("Account created.");
+    die("ADMIN permission assigned.");
 }
