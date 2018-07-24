92162bd1dc1d84b099828b394dd4178914b1f528
Skylar Ittner <admin@netsyms.com>
2018-07-23 21:11:20 -0600

    Improve app dock/bar

--- pages/home.php
+++ pages/home.php
@@ -6,59 +6,58 @@
  */
 ?>
 
-<div class="app-dock-container mobile-app-hide">
-    <div class="app-dock">
+<div class="mobile-app-hide d-flex justify-content-center flex-wrap">
+    <?php
+    foreach (EXTERNAL_APPS as $a) {
+        ?>
+        <div class="app-dock-item m-2">
+            <p class="mb-0">
+                <a href="<?php echo $a['url']; ?>">
+                    <img class="img-responsive app-icon" src="<?php
+                    if (strpos($a['icon'], "http") !== 0) {
+                        echo $a['url'] . $a['icon'];
+                    } else {
+                        echo $a['icon'];
+                    }
+                    ?>"/>
+                    <span class="d-block text-center"><?php echo $a['title']; ?></span>
+                </a>
+            </p>
+        </div>
         <?php
-        foreach (EXTERNAL_APPS as $a) {
-            ?>
-            <div class="app-dock-item">
-                <p>
-                    <a href="<?php echo $a['url']; ?>">
-                        <img class="img-responsive app-icon" src="<?php
-                        if (strpos($a['icon'], "http") !== 0) {
-                            echo $a['url'] . $a['icon'];
-                        } else {
-                            echo $a['icon'];
-                        }
-                        ?>"/>
-                        <span><?php echo $a['title']; ?></span>
-                    </a>
-                </p>
-            </div>
-            <?php
+    }
+    ?>
+</div>
+
+<div class="mobile-app-hide row mt-4">
+    <?php
+    foreach (EXTERNAL_APPS as $a) {
+        if (!isset($a['card'])) {
+            continue;
         }
         ?>
-        <div class="row mt-4">
-            <?php
-            foreach (EXTERNAL_APPS as $a) {
-                if (!isset($a['card'])) {
-                    continue;
-                }
-                ?>
-                <div class="col-12 col-sm-6 col-md-4 mb-4">
-                    <div class="card bg-<?php echo $a['card']['color']; ?> h-100">
-                        <div class="card-body align-middle">
-                            <a href="<?php echo $a['url']; ?>" class="row align-items-center h-100 text-<?php echo (empty($a['card']['text']) ? "light" : $a['card']['text']) ?>">
-                                <div class="col-4 col-sm-4">
-                                    <img class="img-fluid" src="<?php
-                                    if (strpos($a['icon'], "http") !== 0) {
-                                        echo $a['url'] . $a['icon'];
-                                    } else {
-                                        echo $a['icon'];
-                                    }
-                                    ?>"/>
-                                </div>
-                                <div class="col-12 col-sm-8">
-                                    <span class="h5 font-weight-normal"><?php echo $a['title']; ?></span><br />
-                                    <?php $Strings->get($a['card']['string']); ?>
-                                </div>
-                            </a>
+        <div class="col-12 col-sm-6 col-md-4 mb-4">
+            <div class="card bg-<?php echo $a['card']['color']; ?> h-100">
+                <div class="card-body align-middle">
+                    <a href="<?php echo $a['url']; ?>" class="row align-items-center h-100 text-<?php echo (empty($a['card']['text']) ? "light" : $a['card']['text']) ?>">
+                        <div class="col-4 col-sm-4">
+                            <img class="img-fluid" src="<?php
+                            if (strpos($a['icon'], "http") !== 0) {
+                                echo $a['url'] . $a['icon'];
+                            } else {
+                                echo $a['icon'];
+                            }
+                            ?>"/>
                         </div>
-                    </div>
+                        <div class="col-12 col-sm-8">
+                            <span class="h5 font-weight-normal"><?php echo $a['title']; ?></span><br />
+                            <?php $Strings->get($a['card']['string']); ?>
+                        </div>
+                    </a>
                 </div>
-                <?php
-            }
-            ?>
+            </div>
         </div>
-    </div>
+        <?php
+    }
+    ?>
 </div>
