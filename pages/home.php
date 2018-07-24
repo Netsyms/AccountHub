e321b8443448395bbf5a319a46413aee5bf23207
Skylar Ittner <admin@netsyms.com>
2018-07-23 21:29:59 -0600

    Add cards for security and sync pages

--- pages/home.php
+++ pages/home.php
@@ -29,18 +29,18 @@
     ?>
 </div>
 
-<div class="mobile-app-hide row mt-4">
+<div class="row mt-4">
     <?php
     foreach (EXTERNAL_APPS as $a) {
         if (!isset($a['card'])) {
             continue;
         }
         ?>
-        <div class="col-12 col-sm-6 col-md-4 mb-4">
+        <div class="col-12 col-sm-6 col-md-4 mb-4 mobile-app-hide">
             <div class="card bg-<?php echo $a['card']['color']; ?> h-100">
                 <div class="card-body align-middle">
                     <a href="<?php echo $a['url']; ?>" class="row align-items-center h-100 text-<?php echo (empty($a['card']['text']) ? "light" : $a['card']['text']) ?>">
-                        <div class="col-4 col-sm-4">
+                        <div class="col-4">
                             <img class="img-fluid" src="<?php
                             if (strpos($a['icon'], "http") !== 0) {
                                 echo $a['url'] . $a['icon'];
@@ -49,7 +49,7 @@
                             }
                             ?>"/>
                         </div>
-                        <div class="col-12 col-sm-8">
+                        <div class="col-8">
                             <span class="h5 font-weight-normal"><?php echo $a['title']; ?></span><br />
                             <?php $Strings->get($a['card']['string']); ?>
                         </div>
@@ -60,4 +60,34 @@
         <?php
     }
     ?>
+    <div class="col-12 col-sm-6 col-md-4 mb-4">
+        <div class="card bg-orange h-100">
+            <div class="card-body align-middle">
+                <a href="./app.php?page=security" class="row align-items-center h-100 text-dark">
+                    <div class="col-4 text-center">
+                        <i class="fas fa-lock fa-4x"></i>
+                    </div>
+                    <div class="col-8">
+                        <span class="h5 font-weight-normal"><?php $Strings->get("account security"); ?></span><br />
+                        <?php $Strings->get("Change password, setup 2-factor, and change Station PIN"); ?>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 col-sm-6 col-md-4 mb-4">
+        <div class="card bg-orange h-100">
+            <div class="card-body align-middle">
+                <a href="./app.php?page=sync" class="row align-items-center h-100 text-dark">
+                    <div class="col-4 text-center">
+                        <i class="fas fa-sync fa-4x"></i>
+                    </div>
+                    <div class="col-8">
+                        <span class="h5 font-weight-normal"><?php $Strings->get("sync"); ?></span><br />
+                        <?php $Strings->get("Connect mobile devices to AccountHub"); ?>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
 </div>
